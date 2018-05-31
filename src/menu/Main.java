4f52009ff4e1c7dc275967728b83c5a431bc02ad
Gumisia <gabrielaprymaka@icloud.com>
2018-06-01 00:51:45 +0200

    menu done, strzaki, click

--- src/menu/Main.java
+++ src/menu/Main.java
@@ -14,7 +14,7 @@ public class Main extends JFrame{
     public void initComponents(){
 
         this.setTitle("Test Menu");
-        this.setBounds(300, 300, 300, 300);
+        this.setBounds(-700, 300, 300, 300);
 
         panelMenu.setLayout(new GridLayout(3, 1));
 
@@ -31,19 +31,39 @@ public class Main extends JFrame{
     private MenuButton menu1 = new MenuButton("1. Dodaj");
     private MenuButton menu2 = new MenuButton("2. Usuń");
     private MenuButton menu3 = new MenuButton("3. Zmień");
+    private  int i = 0;
 
 
-    private class MenuButton extends JButton implements FocusListener{
+    private class MenuButton extends JButton implements FocusListener, ActionListener{
 
         public MenuButton(String nazwa){
             super(nazwa);
             this.addFocusListener(this);
             this.setOpaque(true);
+            this.addActionListener(this);
+
+//            this.addKeyListener(new KeyListener() {
+//                @Override
+//                // keyPressed -> keyTyped -> keyReleased (naciskam, drukuje, opuszczam)
+//                public void keyTyped(KeyEvent e) {
+//
+//                }
+//
+//                @Override
+//                public void keyPressed(KeyEvent e) {
+//
+//                }
+//
+//                @Override
+//                public void keyReleased(KeyEvent e) {
+//
+//                }
+//            });
 
             this.addKeyListener(new KeyAdapter() {
                 @Override
                 public void keyPressed(KeyEvent e) {
-                    keyPressHandle(e);
+                    keyPressedHandler(e);
                 }
             });
 
@@ -62,11 +82,37 @@ public class Main extends JFrame{
 
         }
 
-        private void keyPressHandle(KeyEvent){
-//            if(e.getKeyC)
+        public void keyPressedHandler(KeyEvent e){
+            int dlMenu = panelMenu.getComponentCount();
+            if(i==0) i = 10 * dlMenu;
+            if(e.getKeyCode() == KeyEvent.VK_DOWN){
+//                0%3 = 0, 1%3 = 1, 2%3 = 2, 3%3 = 0, 4%3 = 1, ...
+                panelMenu.getComponent(++i%dlMenu).requestFocus();
+            }else if(e.getKeyCode() == KeyEvent.VK_UP){
+//                0%3 = 0, 1%3 = 1, 2%3 = 2, 3%3 = 0, 4%3 = 1, ...
+                panelMenu.getComponent(--i%dlMenu).requestFocus();
+            }else if(e.getKeyCode() == KeyEvent.VK_ENTER){
+//                MenuButton tmp = (MenuButton)e.getSource();
+//                tmp.doClick();
+                ((MenuButton)e.getSource()).doClick();
+
+            }
         }
 
+
+
+//        private void keyPressHandle(KeyEvent){
+//        }
+
+        //            if(e.getKeyC)
+        @Override
+        public void actionPerformed(ActionEvent e) {
+//            JOptionPane.showMessageDialog(this, "Cos");
+            JOptionPane.showMessageDialog(this, ((MenuButton)e.getSource()).getText());
+
+        }
         private Color kFocusGained = Color.RED;
+
         private Color kDefault = Color.BLUE;
     }
 
